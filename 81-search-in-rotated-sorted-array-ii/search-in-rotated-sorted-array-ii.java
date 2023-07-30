db46e442217228e091a0e72837a7000f94d4afc5
Chandra Shekhar <100680681+ChandraShekhar1729@users.noreply.github.com>
2023-07-30 20:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (99.33%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,34 +1,45 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        while (start <= end){
-            //to avoid duplicates
-            while(start<end && nums[start]==nums[start+1]){
-                start++;
-            }
-        while(start<end && nums[end]==nums[end-1]){
-                end--;
-            }
-            int mid = start+ (end - start) / 2;
-            if (nums[mid] == target)
-                return true;
+        int low=0;
+        int high=nums.length-1;
+
+        while(low<=high){
+           
+           int mid=(low+high)/2;
+
+           if(nums[mid]==target){
+               return true;
+           }
+           if(nums[mid]==nums[low]&&nums[mid]==nums[high]){
+               low++;
+               high--;
+               continue;
+           }
+           else if(nums[low]<=nums[mid]){
+               if(target>=nums[low]&&target<nums[mid]){
+                   high=mid-1;
+               }
+           
+           else{
+               low=mid+1;
+           }
+           }
+
+           else if(nums[mid]<=nums[high]){
+
+             if(target>nums[mid]&&target<=nums[high]){
+                 low=mid+1;
+             }
+             else{
+                 high=mid-1;
+             }
+
+ }
+
+
+ }
+      
+      return false;
         
-            if (nums[start] <= nums[mid]){
-                 if (target < nums[mid] && target >= nums[start]) 
-                    end = mid - 1;
-                 else
-                    start = mid + 1;
-            } 
-        
-            if (nums[mid] <= nums[end]){
-                if (target > nums[mid] && target <= nums[end])
-                    start = mid + 1;
-                 else
-                    end = mid - 1;
-            }
-        }
-        return false;
     }
-        
-    }
+}
