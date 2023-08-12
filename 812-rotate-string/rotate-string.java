04772791159e00cac9369b86c2fb1c6810322f80
Chandra Shekhar <100680681+ChandraShekhar1729@users.noreply.github.com>
2023-08-12 12:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (87.30%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+         if(s ==null || goal == null){
+            return false;
+        }
+        if(s.length() != goal.length()) return false;
+        if(s.length() == 0){
+            return true;
+        }
+        int i =0, j =0;
+        while(i < s.length() && j < goal.length()){
+            if(s.charAt(i) == goal.charAt(j)){
+                i++; j++;
+            }
+            else{
+                if(j == 0){
+                    i++;
+                }
+                else{
+                    j= 0;
+                }
+            }
+        }
+        return (s.substring(0,goal.length() - j).equals(goal.substring(j)));
+    }
+}
