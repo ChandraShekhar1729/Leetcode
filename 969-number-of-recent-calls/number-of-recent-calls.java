cdf883f4033fdb13723fac2de8d9b4dbc33a44fc
Chandra Shekhar <100680681+ChandraShekhar1729@users.noreply.github.com>
2023-08-25 09:14:58 +0530

    Time: 1379 ms (8.02%) | Memory: 52.4 MB (25.54%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -0,0 +1,21 @@
+class RecentCounter {
+ ArrayList<Integer> calls ;
+    public RecentCounter() {
+        calls = new ArrayList<Integer>();
+    }
+    
+    public int ping(int t) {
+        calls.add(t);
+        int count = 0;
+        for(Integer call:calls){
+            if( t-call<=3000) count++;
+        }
+        return count;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
