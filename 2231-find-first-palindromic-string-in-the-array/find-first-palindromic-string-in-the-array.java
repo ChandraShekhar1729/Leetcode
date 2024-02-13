75e7db13fe371c02d759a8c6f04a14db2a96da61
Chandra Shekhar <100680681+ChandraShekhar1729@users.noreply.github.com>
2024-02-13 23:41:52 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (92.91%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -1,28 +1,47 @@
 class Solution {
-    public static boolean isBoolean(String str){
-         int i=0;int j=str.length()-1;
-
-         while(i<=j){
-             if(str.charAt(i)==str.charAt(j)){
-                 i++;
-                 j--;
-             }
-             else{
-                 return false;
-             }
-         }
-
-         return true;
+    
+    public boolean isPalindromeOrNot(String str){
+        
+        boolean ans=true;
+        
+        int start=0, end=str.length()-1;
+        
+        
+        while(start<=end){
+            
+            if(str.charAt(start)!=str.charAt(end)){
+                ans=false;
+                break;
+            }
+            
+            start++;
+            end--;
+            
+        }
+        
+        return ans;
+        
+        
+        
     }
     public String firstPalindrome(String[] words) {
+        
+        
         for( int i=0;i<words.length;i++){
+            
             String str=words[i];
-            boolean ans=isBoolean(str);
-            if(ans==true){
-                return words[i];
+            if(isPalindromeOrNot(str)==true){
+                
+                return str;
             }
         }
+        
+        
         return "";
+        
+        
+        
+        
         
     }
 }
